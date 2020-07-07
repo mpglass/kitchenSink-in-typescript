27b053e03a03f8951ebcc0870ee29609143960a6
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-07-07 02:54:39 -0500

    lab finished

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
 var myName = "Michelle";
 var numOfStates = 50;
-var doSum = function (x, y) {
+var add = function (x, y) {
     return x + y;
 };
-var add = doSum(5, 4);
+var sum = add(5, 4);
+console.log(sum);
 var sayHello = "Hello World";
 console.log(sayHello);
+function checkAge(name, age) {
+    age > 21 ? console.log("Congrats " + name + ", you are in!") : console.log("Sorry " + name + ", you gotta get older bro!");
+}
+checkAge('Charles', 21);
+checkAge('Abby', 27);
+checkAge('James', 18);
+checkAge('John', 17);
+var veggies = ['broccoli', 'yellow squash', 'onions', 'green beans', 'carrots', 'spinach', 'mushrooms', 'cauliflower'];
+veggies.forEach(function (veggie) {
+    console.log(veggie);
+});
+var pet = {
+    name: 'Lady Fluffington',
+    breed: 'Pembroke Welsh Corgi'
+};
+console.log(pet);
 var friendArray = [
     {
-        name: "Charles",
-        age: 21
+        name: "Noah",
+        age: 16
     },
     {
-        name: "Abby",
+        name: "Emma",
         age: 27
     },
     {
-        name: "James",
+        name: "Liam",
+        age: 29
+    },
+    {
+        name: "Sophia",
         age: 18
     },
     {
-        name: "John",
-        age: 17
+        name: "Jacob",
+        age: 26
     },
 ];
+friendArray.forEach(function (friend) {
+    checkAge(friend.name, friend.age);
+});
+function getLength(word) {
+    var length = word.length;
+    if (length % 2 == 0) {
+        console.log('The world is nice and even!');
+    }
+    else {
+        console.log('The world is an odd place!');
+    }
+}
+getLength('Hello World');
--- app.ts
+++ app.ts
@@ -1,34 +1,80 @@
 let myName: string = "Michelle";
+
 let numOfStates: number = 50;
 
-const doSum = (x: number, y: number): number => {
+
+const add = (x: number, y: number): number => {
   return x + y;
 }
-let add= doSum(5, 4);
+let sum = add(5, 4);
+console.log(sum);
+
 
 const sayHello: string = "Hello World";
 console.log(sayHello);
+   
+
+function checkAge(name: string, age: number): void {
+    age > 21 ? console.log(`Congrats ${name}, you are in!`) : console.log(`Sorry ${name}, you gotta get older bro!`)
+}
+checkAge('Charles', 21); 
+checkAge('Abby', 27); 
+checkAge('James', 18); 
+checkAge('John', 17); 
+
+
+let veggies: string[] = ['broccoli', 'yellow squash', 'onions', 'green beans', 'carrots', 'spinach', 'mushrooms', 'cauliflower']
+veggies.forEach(veggie => {
+    console.log(veggie);
+});
+
+
+let pet: {name: string, breed: string} = {
+    name: 'Lady Fluffington', 
+    breed: 'Pembroke Welsh Corgi'
+};
+console.log(pet);
+
 
 interface IPerson {
-  name: string;
-  age: number;
-}
+    name: string;
+    age: number;
+  }
+  
+  const friendArray: IPerson[] = [
+    {
+      name: "Noah",
+      age: 16,
+    },
+    {
+      name: "Emma",
+      age: 27,
+    },
+    {
+      name: "Liam",
+      age: 29,
+    },
+    {
+      name: "Sophia",
+      age: 18,
+    },
+    {
+      name: "Jacob",
+      age: 26,
+    },
+  ];
 
-const friendArray: IPerson[] = [
-  {
-    name: "Charles",
-    age: 21,
-  },
-  {
-    name: "Abby",
-    age: 27,
-  },
-  {
-    name: "James",
-    age: 18,
-  },
-  {
-    name: "John",
-    age: 17,
-  },
-];
+friendArray.forEach(friend => {
+    checkAge(friend.name, friend.age);
+});
+
+function getLength (word: string) {
+    var length: number = word.length
+    if(length % 2 == 0 ) {
+        console.log('The world is nice and even!')
+    }
+    else {
+        console.log('The world is an odd place!')
+    }
+}
+getLength('Hello World')
